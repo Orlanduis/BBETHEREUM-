8a22e84a1f97b7140117255d5d0beeb7daf0af88
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-11-01 01:06:27 -0600

    Create fork_node.js

---
+++ fork_node.js
@@ -0,0 +1,10 @@
+networks: "Optimism"
+  hardhat: {
+    forking: {
+      url: "https://rpc.buildbear.io/esquivelfabian,"https://optimism.llamarpc.com".
+        httpHeaders: {
+        "Authorization": "Bearer <O1JBK81AJSGJ9PZDZRG13AMM5M>
+      }
+    }
+  }
+}
