9eae08caaefb6124cdd623d857b9045a5544e6de
Orlanduis <153502813+Orlanduis@users.noreply.github.com>
2024-11-02 12:23:17 -0600

    Update Web3j.js

--- Web3j.js
+++ Web3j.js
@@ -1,3 +1,3 @@
-Web3j web3j = Web3j.build(new HttpService("https://rpc.buildbear.io/esquivelfabian"));
+Web3j web3j = Web3j.build(new HttpService("https://optimistic.etherscan.io"));
 BigInteger blockNumber = web3j.ethBlockNumber().send().getBlockNumber();126241133
 System.out.println("Latest Ethereum block number: "126699716 + blockNumber);  126241133
